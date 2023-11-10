630dcde39d088f7570e9cf562eab3448e454a4bd
Alexander Markin <alexander.markin@creamteam.de>
2023-11-10 14:20:33 +0100

    registration page

--- users-app/ui/src/app/pages/registration-page/registration-page.component.ts
+++ users-app/ui/src/app/pages/registration-page/registration-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Profession } from 'src/app/models/profession.model';
+import { Quality } from 'src/app/models/quality.model';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.css']
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit, OnDestroy{
+
+  professionsList: Profession[] = [];
+  qualitiesList: Quality[] = [];
+
+  dataSub = new Subscription();
+
+  constructor(
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.dataSub = this.activatedRoute.data.subscribe(({0: qualities, 1: professions}) => {
+      this.qualitiesList = qualities;
+      this.professionsList = professions;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.dataSub.unsubscribe();
+  }
+
+
 
 }
